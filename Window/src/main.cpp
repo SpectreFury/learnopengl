f52e217de4f6555945dee3c0b5f3945dadd62f4b
Ayush Soni <soni.ayush.2212@gmail.com>
2025-08-31 17:51:15 +0530

    Add: Window lesson

--- Window/src/main.cpp
+++ Window/src/main.cpp
@@ -1,6 +1,48 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main() {
-  std::cout << "Window\n";
-  return 0;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
+                                        "OpenGL Window", NULL, NULL);
+
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    glClearColor(0.5, 1, 1, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+  }
+
+  glfwTerminate();
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
